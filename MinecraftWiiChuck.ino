9c7f8de2e2b33cad6fac256d79bec167fee0fce1
miznash <sugino41@gmail.com>
2013-01-06 15:00:47 +0900

    First commit.

---
+++ MinecraftWiiChuck.ino
@@ -0,0 +1,508 @@
+/*
+ * MinecraftWiiChuck
+ *
+ * 2013 miznash
+ *
+ * Let's play minecraft with Arduino Leonard + Wii nunchuck!
+ * ------------------------------------------------------------------
+ * THE SOFTWARE IS PROVIDED ÅgAS ISÅh, WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED.
+ * ------------------------------------------------------------------
+ *
+ *  <Keyboard operation>
+ *  A,W,S,D ... Joystick
+ *  Space   ... Z
+ *  E       ... C + Z
+ *  Q       ... C + Shake Wii Nunchuck
+ *
+ *  <Mouse operation>
+ *  Move        ... Wii Nunchuck roll & pitch
+ *  Left click  ... Shake Wii Nunchuck
+ *  Right click ... C (When C button is released) 
+ *  Mouse wheel ... C + Roll joystick
+ *  
+ *  <Get / Release Mouse & Keyboard> 
+ *  C + Z + Joystick down
+ *  (You need to do this operation first when this program is started.)
+ *
+ */
+
+#include <Wire.h>
+#include "WiiChuck.h"
+
+typedef boolean buttonState;
+#define BUTTON_PRESSED   true
+#define BUTTON_RELEASED  false
+
+#define MAIN_LOOP_INTERVAL_MSEC    30
+#define THRESH_SHAKE               50
+#define SHAKE_SENSITIVITY           5
+#define THRESH_MOUSE_MOVE          16
+#define CONTROL_ON_OFF_SENSITIVITY 10 // x MAIN_LOOP_INTERVAL_MSEC is needed for on/off control
+
+#define JOY_PRESS_THRESH_SOFT   30
+#define JOY_PRESS_THRESH_NORMAL 60
+
+WiiChuck chuck = WiiChuck();
+
+boolean isJumping = false;
+boolean skipCbuttonReleased = false;
+
+JOY_POS lastJoyPosLR = JOY_POS_NEUTRAL;
+JOY_POS joyPosLR     = JOY_POS_NEUTRAL;
+JOY_POS lastJoyPosUD = JOY_POS_NEUTRAL;
+JOY_POS joyPosUD     = JOY_POS_NEUTRAL;
+boolean joyPressSoft = false;
+
+int lastJoyPosForWheel = -1;
+int joyPosForWheel = -1;
+
+int lpfAccelX = 0;
+int lpfAccelY = 0;
+int lpfAccelZ = 0;
+int lastAccelX = 0;
+int lastAccelY = 0;
+int lastAccelZ = 0;
+
+void setup()
+{
+  Serial.begin(19200);
+  while (!Serial) { 
+    ; 
+  }  // Waiting serial ready (for Arduino Leonard)
+  chuck.begin();
+  chuck.update();
+  Mouse.begin();
+  Keyboard.begin();
+}
+
+void onCbuttonPressed()
+{
+  Serial.println("C pressed");
+}
+
+void onCbuttonReleased()
+{
+  if (!skipCbuttonReleased) {
+    Mouse.press(MOUSE_RIGHT);
+    Mouse.release(MOUSE_RIGHT);
+    Serial.println("C released");
+  }
+  else {
+    skipCbuttonReleased = false;
+  }
+}
+
+void onZbuttonPressed()
+{
+  if (!isJumping) {
+    Keyboard.press(' ');
+    isJumping = true; 
+  }
+}
+
+void onZbuttonReleased()
+{
+  if (isJumping) {   
+    Keyboard.release(' ');
+    isJumping = false;
+  }
+}
+
+void onCZbuttonsPressed()
+{
+  Keyboard.press('e');
+  Keyboard.release('e');
+  Serial.println("C and Z pressed");
+  skipCbuttonReleased = true;
+}
+
+void mouseEmulation()
+{
+  static int shakeSensitivityCounter = 0;
+  int mouseMoveStepLR = 0;
+  int mouseMoveStepUD = 0;
+
+  if (abs(lpfAccelZ - chuck.readAccelZ()) > THRESH_SHAKE) {
+    if (chuck.isCbuttonPressed()) {
+      // C button press + shake = 'Q' throw item
+      Keyboard.press('q');
+      Keyboard.release('q');
+      skipCbuttonReleased = true;
+    }
+    else {
+      Mouse.press(MOUSE_LEFT);
+      shakeSensitivityCounter = SHAKE_SENSITIVITY;
+    }
+  }
+  else {
+    if (shakeSensitivityCounter > 0) {
+      shakeSensitivityCounter--;
+    }
+    if (shakeSensitivityCounter == 0) {
+      Mouse.release(MOUSE_LEFT);
+    }
+    // Change player's sight (Mouse move)
+    if (!chuck.isZbuttonPressed() && shakeSensitivityCounter == 0) {
+      mouseMoveStepLR = chuck.readRoll();
+      mouseMoveStepLR = (abs(mouseMoveStepLR) > THRESH_MOUSE_MOVE ? (mouseMoveStepLR >> 2) : 0);
+      mouseMoveStepUD = -(chuck.readPitch() - 90);  // Pitch 90 degrees = even with the ground
+      mouseMoveStepUD = (abs(mouseMoveStepUD) > THRESH_MOUSE_MOVE ? (mouseMoveStepUD >> 2) : 0);
+      Mouse.move(mouseMoveStepLR, mouseMoveStepUD, 0);
+    }
+  }
+
+  // Mouse Wheel = Roll JoyStick
+  mouseWheelEmulation();
+}
+
+void mouseWheelEmulation()
+{
+  // The direction of joystick correspond to the index number.
+  // If last position is 0 and current position is 2,
+  // wheelStepTable[0][2] = 2. It means wheeling mouse 2 steps.
+  //
+  //        2
+  //     1  |  3
+  //   0  --+--  4
+  //     7  |  5
+  //        6
+  static signed char wheelStepTable[8][8] = { // [last][current]
+    0,  1,  2,  3,  0, -3, -2, -1,
+    -1,  0,  1,  2,  3,  0, -3, -2,
+    -2, -1,  0,  1,  2,  3,  0, -3,
+    -3, -2, -1,  0,  1,  2,  3,  0,
+    0, -3, -2, -1,  0,  1,  2,  3,
+    3,  0, -3, -2, -1,  0,  1,  2,
+    2,  3,  0, -3, -2, -1,  0,  1,
+    1,  2,  3,  0, -3, -2, -1,  0
+  };
+
+  signed char wheelStep;
+  if (chuck.isCbuttonPressed()) {
+    if (joyPosForWheel != -1) {
+      skipCbuttonReleased = true;
+      if (lastJoyPosForWheel != -1) {
+        wheelStep = wheelStepTable[lastJoyPosForWheel][joyPosForWheel];
+        Mouse.move(0, 0, wheelStep);
+      }
+    }
+  }
+}
+
+void updateJoyPosForWheel()
+{
+  lastJoyPosForWheel = joyPosForWheel;
+
+  switch (chuck.getJoyPosLR()) {
+  case JOY_POS_NEUTRAL:
+    switch (chuck.getJoyPosUD()) {
+    case JOY_POS_NEUTRAL:
+      joyPosForWheel = -1;
+      break;
+    case JOY_POS_UP:
+      joyPosForWheel = 2;
+      break;
+    case JOY_POS_DOWN:
+      joyPosForWheel = 6;
+      break;
+    default:
+      ;
+    }
+    break;
+  case JOY_POS_LEFT:
+    switch (chuck.getJoyPosUD()) {
+    case JOY_POS_NEUTRAL:
+      joyPosForWheel = -1;
+      break;
+    case JOY_POS_UP:
+      joyPosForWheel = 1;
+      break;
+    case JOY_POS_DOWN:
+      joyPosForWheel = 7;
+      break;
+    default:
+      ;
+    }
+    break;
+  case JOY_POS_RIGHT:
+    switch (chuck.getJoyPosUD()) {
+    case JOY_POS_NEUTRAL:
+      joyPosForWheel = 4;
+      break;
+    case JOY_POS_UP:
+      joyPosForWheel = 3;
+      break;
+    case JOY_POS_DOWN:
+      joyPosForWheel = 5;
+      break;
+    default:
+      break;
+    }
+    break;
+  default:
+    ;
+  } 
+}
+
+void onJoyPosNeutral()
+{
+  Serial.println("onJoyPosNeutral");
+  switch (lastJoyPosLR) {
+  case JOY_POS_LEFT:
+    Keyboard.release('a');
+    break;
+  case JOY_POS_RIGHT:
+    Keyboard.release('d');
+    break;
+  case JOY_POS_NEUTRAL:
+    break;
+  default:
+    ;
+  }
+  switch (lastJoyPosUD) {
+  case JOY_POS_UP:
+    Keyboard.release('w');
+    break;
+  case JOY_POS_DOWN:
+    Keyboard.release('s');
+    break;
+  case JOY_POS_NEUTRAL:
+    break;
+  default:
+    ;
+  }
+
+}
+
+void onJoyPosUp()
+{
+  switch (lastJoyPosUD) {
+  case JOY_POS_UP:
+    break;
+  case JOY_POS_DOWN:
+    Keyboard.release('s');
+    Keyboard.press('w');
+    break;
+  case JOY_POS_NEUTRAL:
+    Keyboard.press('w');
+    break;
+  default:
+    ;
+  }
+  Serial.println("onJoyPosUp");
+}
+
+void onJoyPosDown()
+{
+  switch (lastJoyPosUD) {
+  case JOY_POS_UP:
+    Keyboard.release('w');
+    Keyboard.press('s');
+    break;
+  case JOY_POS_DOWN:
+    break;
+  case JOY_POS_NEUTRAL:
+    Keyboard.press('s');
+    break;
+  default:
+    ;
+  }
+  Serial.println("onJoyPosDown");
+}
+
+void onJoyPosRight()
+{
+  Serial.println("onJoyPosRight");
+  switch (lastJoyPosLR) {
+  case JOY_POS_LEFT:
+    Keyboard.release('a');
+    Keyboard.press('d');
+    break;
+  case JOY_POS_RIGHT:
+    break;
+  case JOY_POS_NEUTRAL:
+    Keyboard.press('d');
+    break;
+  default:
+    ;
+  }
+}
+
+void onJoyPosLeft()
+{
+  Serial.println("onJoyPosLeft");
+  switch (lastJoyPosLR) {
+  case JOY_POS_LEFT:
+    break;
+  case JOY_POS_RIGHT:
+    Keyboard.release('d');
+    Keyboard.press('a');
+    break;
+  case JOY_POS_NEUTRAL:
+    Keyboard.press('a');
+    break;
+  default:
+    ;
+  }
+}
+
+
+void fireButtonEvent()
+{
+  static buttonState lastCbutton = BUTTON_RELEASED;
+  static buttonState lastZbutton = BUTTON_RELEASED;
+  static buttonState cButton = BUTTON_RELEASED;
+  static buttonState zButton = BUTTON_RELEASED;
+
+  lastCbutton = cButton;
+  cButton = (chuck.isCbuttonPressed() ? BUTTON_PRESSED : BUTTON_RELEASED);
+  lastZbutton = zButton;
+  zButton = (chuck.isZbuttonPressed() ? BUTTON_PRESSED : BUTTON_RELEASED);
+
+  // fire event
+  if (lastCbutton == BUTTON_RELEASED && cButton == BUTTON_PRESSED) {
+    onCbuttonPressed();
+  }
+  if (lastCbutton == BUTTON_PRESSED && cButton == BUTTON_RELEASED) {
+    onCbuttonReleased();
+  }
+  if (lastZbutton == BUTTON_RELEASED && zButton == BUTTON_PRESSED) {
+    onZbuttonPressed();
+  }
+  if (lastZbutton == BUTTON_PRESSED && zButton == BUTTON_RELEASED) {
+    onZbuttonReleased();
+  }
+  if ((cButton == BUTTON_PRESSED && zButton == BUTTON_PRESSED)
+    && (lastCbutton == BUTTON_RELEASED || lastZbutton == BUTTON_RELEASED)) {
+    onCZbuttonsPressed();
+  }
+}
+
+void fireJoystickEvent()
+{
+  boolean fireJoyPosNeutral = false;
+  lastJoyPosUD = joyPosUD;
+  lastJoyPosLR = joyPosLR;
+
+  joyPressSoft = false;
+  joyPosUD = chuck.getJoyPosUD(JOY_PRESS_THRESH_NORMAL);
+  joyPosLR = chuck.getJoyPosLR(JOY_PRESS_THRESH_NORMAL);
+  if (joyPosUD == JOY_POS_NEUTRAL
+    && joyPosLR == JOY_POS_NEUTRAL) {
+    joyPosUD = chuck.getJoyPosUD(JOY_PRESS_THRESH_SOFT);
+    joyPosLR = chuck.getJoyPosLR(JOY_PRESS_THRESH_SOFT);
+    if (joyPosUD != JOY_POS_NEUTRAL
+      || joyPosLR != JOY_POS_NEUTRAL) {
+      joyPressSoft = true;
+    }
+  }
+
+  // To prevent from player's move when mouse wheeling,
+  // player cannot move when C button is pressed.
+  if (!chuck.isCbuttonPressed()) {
+    if (joyPressSoft) {
+      Keyboard.press(KEY_LEFT_SHIFT);
+    }
+    else {
+      Keyboard.release(KEY_LEFT_SHIFT);
+    }
+
+    switch (joyPosUD) {
+    case JOY_POS_NEUTRAL:
+      if (joyPosUD != lastJoyPosUD) {
+        fireJoyPosNeutral = true;
+      }
+      break;
+    case JOY_POS_UP:
+      if (joyPosUD != lastJoyPosUD) {
+        onJoyPosUp();
+      }
+      break;
+    case JOY_POS_DOWN:
+      if (joyPosUD != lastJoyPosUD) {
+        onJoyPosDown();
+      }
+      break;
+    default:
+      ;
+    }
+    switch (joyPosLR) {
+    case JOY_POS_NEUTRAL:
+      if (joyPosLR != lastJoyPosLR) {
+        fireJoyPosNeutral = true;
+      }
+      break;
+    case JOY_POS_LEFT:
+      if (joyPosLR != lastJoyPosLR) {
+        onJoyPosLeft();
+      }
+      break;
+    case JOY_POS_RIGHT:
+      if (joyPosLR != lastJoyPosLR) {
+        onJoyPosRight();
+      }
+      break;
+    default:
+      ;
+    }
+    if (fireJoyPosNeutral) {
+      onJoyPosNeutral();
+    }
+  }
+
+  updateJoyPosForWheel();
+}
+
+
+void loop()
+{
+  static boolean isInControl = false;  // Mouse & Keyboard control from Arduino
+  static int controlOnOffCounter = 0;
+  static int loop_cnt = 0;
+
+  if (loop_cnt > MAIN_LOOP_INTERVAL_MSEC) { // every 30 msecs get new data
+    loop_cnt = 0;
+
+    chuck.update(); 
+
+    // for releasing mouse & keyboard control
+    if (chuck.isZbuttonPressed() && chuck.isCbuttonPressed() && chuck.isJoyStickDown()) {
+      controlOnOffCounter++;
+      if (controlOnOffCounter > CONTROL_ON_OFF_SENSITIVITY) {
+        if (isInControl) {
+          Keyboard.releaseAll();
+          Mouse.release(MOUSE_LEFT | MOUSE_RIGHT);
+          Serial.print("Control end.");
+        }
+        else {
+          Serial.print("Control start.");
+        }
+        isInControl = !isInControl;
+        controlOnOffCounter = 0;
+      }
+    }
+    else {
+      controlOnOffCounter = 0;
+    }
+
+
+    if (isInControl) {
+      fireButtonEvent();
+      fireJoystickEvent();
+      mouseEmulation();
+    }
+
+    lastAccelX = chuck.readAccelX();
+    lastAccelY = chuck.readAccelY();
+    lastAccelZ = chuck.readAccelZ();
+
+    lpfAccelX = lastAccelX * 0.9 + (float)lpfAccelX * 0.1;
+    lpfAccelY = lastAccelY * 0.9 + (float)lpfAccelY * 0.1;
+    lpfAccelZ = lastAccelZ * 0.9 + (float)lpfAccelZ * 0.1;
+  }
+  loop_cnt++;
+  delay(1);
+}
+
+
---
+++ WiiChuck.h
@@ -0,0 +1,278 @@
+/*
+ * WiiChuck class for playing Minecraft
+ * 2013 miznash
+ *
+ * The original code is Tim Hirzel's WiiChuck class and it's an adaptation of
+ * the code by Tod E. Kurt.
+ * http://playground.arduino.cc/Main/WiiChuckClass
+ *
+ * ------------------------------------------------------------------
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED.
+ * ------------------------------------------------------------------
+ *
+ *
+ * Nunchuck -- Use a Wii Nunchuck
+ * Tim Hirzel http://www.growdown.com
+ * 
+ notes on Wii Nunchuck Behavior.
+ This library provides an improved derivation of rotation angles from the nunchuck accelerometer data.
+ The biggest different over existing libraries (that I know of ) is the full 360 degrees of Roll data
+ from teh combination of the x and z axis accelerometer data using the math library atan2. 
+ 
+ It is accurate with 360 degrees of roll (rotation around axis coming out of the c button, the front of the wii),
+ and about 180 degrees of pitch (rotation about the axis coming out of the side of the wii).  (read more below)
+ 
+ In terms of mapping the wii position to angles, its important to note that while the Nunchuck
+ sense Pitch, and Roll, it does not sense Yaw, or the compass direction.  This creates an important
+ disparity where the nunchuck only works within one hemisphere.  At a result, when the pitch values are 
+ less than about 10, and greater than about 170, the Roll data gets very unstable.  essentially, the roll
+ data flips over 180 degrees very quickly.   To understand this property better, rotate the wii around the
+ axis of the joystick.  You see the sensor data stays constant (with noise).  Because of this, it cant know
+ the difference between arriving upside via 180 degree Roll, or 180 degree pitch.  It just assumes its always
+ 180 roll.
+ 
+ 
+ * 
+ * This file is an adaptation of the code by these authors:
+ * Tod E. Kurt, http://todbot.com/blog/
+ *
+ * The Wii Nunchuck reading code is taken from Windmeadow Labs
+ * http://www.windmeadow.com/node/42
+ */
+
+#ifndef WiiChuck_h
+#define WiiChuck_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Wire.h>
+#include <math.h>
+
+
+// these may need to be adjusted for each nunchuck for calibration
+#define ZEROX 510  
+#define ZEROY 490
+//#define ZEROZ 720
+#define ZEROZ 460
+#define RADIUS 210  // probably pretty universal
+
+#define DEFAULT_ZERO_JOY_X 124
+#define DEFAULT_ZERO_JOY_Y 132
+
+enum JOY_POS {
+    JOY_POS_NEUTRAL = 0,
+    JOY_POS_LEFT = 1,
+    JOY_POS_UP = 1,
+    JOY_POS_RIGHT = -1,
+    JOY_POS_DOWN = -1
+};
+
+class WiiChuck {
+public:
+
+private:
+  byte cnt;
+  uint8_t status[6];		// array to store wiichuck output
+  byte averageCounter;
+  //int accelArray[3][AVERAGE_N];  // X,Y,Z
+  int i;
+  int total;
+  uint8_t zeroJoyX;   // these are about where mine are
+  uint8_t zeroJoyY; // use calibrateJoy when the stick is at zero to correct
+  int lastJoyX;
+  int lastJoyY;
+  int angles[3];
+
+  boolean lastZ, lastC;
+
+
+public:
+
+  byte joyX;
+  byte joyY;
+  boolean buttonZ;
+  boolean buttonC;
+  void begin() 
+  {
+    Wire.begin();
+    cnt = 0;
+    averageCounter = 0;
+    Wire.begin();                // join i2c bus as master
+    Wire.beginTransmission(0x52);// transmit to device 0x52
+#if (ARDUINO >= 100)
+    Wire.write((uint8_t)0x40);// sends memory address
+    Wire.write((uint8_t)0x00);// sends sent a zero.  
+#else
+    Wire.send((uint8_t)0x40);// sends memory address
+    Wire.send((uint8_t)0x00);// sends sent a zero.  
+#endif
+    Wire.endTransmission();// stop transmitting
+    update();            
+    for (i = 0; i<3;i++) {
+      angles[i] = 0;
+    }
+    zeroJoyX = DEFAULT_ZERO_JOY_X;
+    zeroJoyY = DEFAULT_ZERO_JOY_Y;
+  }
+
+
+  void calibrateJoy() {
+    zeroJoyX = joyX;
+    zeroJoyY = joyY;
+  }
+
+  void update() {
+
+    Wire.requestFrom (0x52, 6);	// request data from nunchuck
+    while (Wire.available ()) {
+      // receive byte as an integer
+#if (ARDUINO >= 100)
+      status[cnt] = _nunchuk_decode_byte( Wire.read() );
+#else
+      status[cnt] = _nunchuk_decode_byte( Wire.receive() );
+#endif
+      cnt++;
+    }
+    if (cnt > 5) {
+      lastZ = buttonZ;
+      lastC = buttonC;
+      lastJoyX = readJoyX();
+      lastJoyY = readJoyY();
+
+      cnt = 0;
+      joyX = (status[0]);
+      joyY = (status[1]);
+      for (i = 0; i < 3; i++) {
+        //angles[i] = (status[i+2] << 2) + ((status[5] & (B00000011 << ((i+1)*2) ) >> ((i+1)*2))); 
+        angles[i] = (status[i+2] << 2) + (((status[5] & (B00000011 << ((i+1)*2) )) >> ((i+1)*2))); 
+      }
+
+      buttonZ = !( status[5] & B00000001);
+      buttonC = !((status[5] & B00000010) >> 1);
+      _send_zero(); // send the request for next bytes
+    }
+  }
+
+  float readAccelX() {
+    // total = 0; // accelArray[xyz][averageCounter] * FAST_WEIGHT;
+    return (float)angles[0] - ZEROX;
+  }
+  float readAccelY() {
+    // total = 0; // accelArray[xyz][averageCounter] * FAST_WEIGHT;
+    return (float)angles[1] - ZEROY;
+  }
+  float readAccelZ() {
+    // total = 0; // accelArray[xyz][averageCounter] * FAST_WEIGHT;
+    return (float)angles[2] - ZEROZ;
+  }
+
+  boolean zPressed() {
+    return (buttonZ && ! lastZ);
+  }
+  boolean isCbuttonPressed() {
+    return buttonC;
+  }
+  boolean isZbuttonPressed() {
+    return buttonZ;    
+  }
+
+  boolean isJoyPressed(int thresh=60) {
+    return (isJoyStickUp(thresh) | isJoyStickDown(thresh) | isJoyStickLeft(thresh) | isJoyStickRight(thresh));
+  }
+  // for using the joystick like a directional button
+  boolean isJoyStickRight(int thresh=60) {
+    return (readJoyX() > thresh);
+  }
+
+  // for using the joystick like a directional button
+  boolean isJoyStickLeft(int thresh=60) {
+    return (readJoyX() < -thresh);
+  }
+
+  // for using the joystick like a directional button
+  boolean isJoyStickUp(int thresh=60) {
+    return (readJoyY() > thresh);
+  }
+
+  // for using the joystick like a directional button
+  boolean isJoyStickDown(int thresh=60) {
+    return (readJoyY() < -thresh);
+  }
+
+  JOY_POS getJoyPosLR(int thresh = 60) {
+    if (isJoyStickLeft(thresh)) {
+      return JOY_POS_LEFT;
+    }
+    else if (isJoyStickRight(thresh)) {
+      return JOY_POS_RIGHT;      
+    }
+    else {
+      return JOY_POS_NEUTRAL;
+    }
+  }
+
+  JOY_POS getJoyPosUD(int thresh = 60) {
+    if (isJoyStickUp(thresh)) {
+      return JOY_POS_UP;
+    }
+    else if (isJoyStickDown(thresh)) {
+      return JOY_POS_DOWN;      
+    }
+    else {
+      return JOY_POS_NEUTRAL;
+    }
+  }
+
+  int readJoyX() {
+    return (int) joyX - zeroJoyX;
+  }
+
+  int readJoyY() {
+    return (int)joyY - zeroJoyY;
+  }
+
+
+  // R, the radius, generally hovers around 210 (at least it does with mine)
+  // int R() {
+  //     return sqrt(readAccelX() * readAccelX() +readAccelY() * readAccelY() + readAccelZ() * readAccelZ());  
+  // }
+
+
+  // returns roll degrees
+  int readRoll() {
+    return (int)(atan2(readAccelX(),readAccelZ())/ M_PI * 180.0);
+  }
+
+  // returns pitch in degrees
+  int readPitch() {        
+    return (int) (acos(readAccelY()/RADIUS)/ M_PI * 180.0);  // optionally swap 'RADIUS' for 'R()'
+  }
+
+private:
+  byte _nunchuk_decode_byte (byte x)
+  {
+    x = (x ^ 0x17) + 0x17;
+    return x;
+  }
+
+  void _send_zero()
+  {
+    Wire.beginTransmission (0x52);	// transmit to device 0x52
+#if (ARDUINO >= 100)
+    Wire.write((uint8_t)0x00);// sends one byte
+#else
+    Wire.send((uint8_t)0x00);// sends one byte
+#endif
+    Wire.endTransmission ();	// stop transmitting
+  }
+
+};
+#endif
+
+
+
